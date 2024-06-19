590a645ad3e5bddbe457e814b0a7b28cb8711f7d
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-19 17:48:15 +0300

    main

--- Lesson20-BankCard/Lesson20-BankCard.cpp
+++ Lesson20-BankCard/Lesson20-BankCard.cpp
@@ -2,8 +2,17 @@
 
 int main()
 {
-    Card privat;
+    Card* mono = nullptr;
 
-    privat.showInfo();
+    bool user;
+    cout << "0 - Standart card\n";
+    cout << "1 - VIP card\n";
+    cin >> user;
+    if (user == false) mono = new Card();
+    else mono = new Card("Lapshun Tetiana", 25'000);
+
+    mono->showInfo();
+
+    delete mono;
 }
 
